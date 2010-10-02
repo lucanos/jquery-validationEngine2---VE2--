1d8dfda14360b599f9ef48070a38c7a077d31793
Luke Stevenson <lucanos@gmail.com>
2010-10-03 01:25:57 +1000

    Further formatting of javascript.

--- js/jquery.validationEngine-en.js
+++ js/jquery.validationEngine-en.js
@@ -1,67 +1,86 @@
 
 
 (function($) {
-	$.fn.validationEngineLanguage = function() {};
-	$.validationEngineLanguage = {
-		newLang: function() {
-			$.validationEngineLanguage.allRules = 	{"required":{    			// Add your regex rules here, you can take telephone as an example
-						"regex":"none",
-						"alertText":"* This field is required",
-						"alertTextCheckboxMultiple":"* Please select an option",
-						"alertTextCheckboxSingle":"* This checkbox is required"},
-					"length":{
-						"regex":"none",
-						"alertText":"* Between",
-						"alertText2":"and",
-						"alertText3": "characters allowed"},
-					"maxCheckbox":{
-						"regex":"none",
-						"alertText":"* Checks allowed Exceeded"},	
-					"minCheckbox":{
-						"regex":"none",
-						"alertText":"* Please select ",
-						"alertText2":" options"},	
-					"confirm":{
-						"regex":"none",
-						"alertText":"* Your fields do not match"},		
-					"telephone":{
-						"regex":"/^[0-9\-\(\)\ ]+$/",
-						"alertText":"* Invalid phone number"},	
-					"email":{
-						"regex":"/^[a-zA-Z0-9_\.\-]+\@([a-zA-Z0-9\-]+\.)+[a-zA-Z0-9]{2,4}$/",
-						"alertText":"* Invalid email address"},	
-					"date":{
-            "regex":"/^(?:[012]?[0-9]|3[01])[\/\-](?:0?[0-9]|1[0-2])[\/\-](?:[12][0-9]{3})$/",
-            "alertText":"* Invalid date, must be in DD/MM/YYYY format"},
-					"onlyNumber":{
-						"regex":"/^[0-9\ ]+$/",
-						"alertText":"* Numbers only"},	
-					"noSpecialCharacters":{
-						"regex":"/^[0-9a-zA-Z]+$/",
-						"alertText":"* No special characters allowed"},	
-					"ajaxUser":{
-						"file":"validateUser.php",
-						"extraData":"name=eric",
-						"alertTextOk":"* This user is available",	
-						"alertTextLoad":"* Loading, please wait",
-						"alertText":"* This user is already taken"},	
-					"ajaxName":{
-						"file":"validateUser.php",
-						"alertText":"* This name is already taken",
-						"alertTextOk":"* This name is available",	
-						"alertTextLoad":"* Loading, please wait"},		
-					"onlyLetter":{
-						"regex":"/^[a-zA-Z\ \']+$/",
-						"alertText":"* Letters only"},
-					"validate2fields":{
-    					"nname":"validate2fields",
-    					"alertText":"* You must have a firstname and a lastname"}	
-					}	
-					
-		}
-	}
+
+  $.fn.validationEngineLanguage = function() {};
+
+  $.validationEngineLanguage = {
+    newLang :  function() {
+      $.validationEngineLanguage.allRules = { // Add your regex rules here, you can take telephone as an example
+        "required" : {
+          "regex" : "none",
+          "alertText" : "* This field is required",
+          "alertTextCheckboxMultiple" : "* Please select an option",
+          "alertTextCheckboxSingle" : "* This checkbox is required"
+        } ,
+        "length" : {
+          "regex"      : "none",
+          "alertText"  : "* Between",
+          "alertText2" : "and",
+          "alertText3" : "characters allowed"
+        } ,
+        "maxCheckbox" : {
+          "regex"     : "none",
+          "alertText" : "* Checks allowed Exceeded"
+        } ,
+        "minCheckbox" : {
+          "regex"      : "none",
+          "alertText"  : "* Please select ",
+          "alertText2" : " options"
+        } ,
+        "confirm" : {
+          "regex"     : "none",
+          "alertText" : "* Your fields do not match"
+        } ,
+        "telephone" : {
+          "regex"     : "/^[0-9\-\(\)\ ]+$/",
+          "alertText" : "* Invalid phone number"
+        } ,
+        "email" : {
+          "regex"     : "/^[a-zA-Z0-9_\.\-]+\@([a-zA-Z0-9\-]+\.)+[a-zA-Z0-9]{2,4}$/",
+          "alertText" : "* Invalid email address"
+        } ,
+        "date" : {
+          "regex"     : "/^(?:[012]?[0-9]|3[01])[\/\-](?:0?[0-9]|1[0-2])[\/\-](?:[12][0-9]{3})$/",
+          "alertText" : "* Invalid date, must be in DD/MM/YYYY format"
+        } ,
+        "onlyNumber" : {
+          "regex"     : "/^[0-9\ ]*$/",
+          "alertText" : "* Numbers only"
+        } ,
+        "noSpecialCharacters" : {
+          "regex"     : "/^[0-9a-zA-Z]*$/",
+          "alertText" : "* No special characters allowed"
+        } ,
+        "ajaxUser" : {
+          "file"          : "validateUser.php",
+          "extraData"     : "name=eric",
+          "alertTextOk"   : "* This user is available",
+          "alertTextLoad" : "* Loading, please wait",
+          "alertText"     : "* This user is already taken"
+        } ,
+        "ajaxName" : {
+          "file"          : "validateUser.php",
+          "alertText"     : "* This name is already taken",
+          "alertTextOk"   : "* This name is available",
+          "alertTextLoad" : "* Loading, please wait"
+        } ,
+        "onlyLetter" : {
+          "regex"     : "/^[a-zA-Z\ \']*$/",
+          "alertText" : "* Letters only"
+        } ,
+        "validate2fields" : {
+          "nname"     : "validate2fields",
+          "alertText" : "* You must have a firstname and a lastname"
+        }
+      }
+    }
+  }
+
 })(jQuery);
 
-$(document).ready(function() {	
-	$.validationEngineLanguage.newLang()
-});
+$( document ).ready( function(){
+
+  $.validationEngineLanguage.newLang();
+
+} );
