2994629905064a587959aca19de5eb9b38269b5d
Luke Stevenson <lucanos@gmail.com>
2010-10-02 11:46:32 +1000

    Formatted PHP files.

--- ajaxSubmit.php
+++ ajaxSubmit.php
@@ -1,34 +1,33 @@
 <?php
 
 /*  RECEIVE POST   */
-$email=$_POST['email'];
-$age=$_POST['age'];
+$email = $_POST['email'];
+$age   = $_POST['age'];
 
 /* VALIDATE HOW YOU NEED TO VALIDATE */
 
 
-
-
 /* RETURN ERROR */
-
-$arrayError[0][0] = "#email";			// FIELDID 
-$arrayError[0][1] = "Your email do not match.. whatever it need to match"; 	// TEXT ERROR	
-$arrayError[0][2] = "error";			// BOX COLOR
-
-$arrayError[1][0] = "#age";		// FIELD
-$arrayError[1][1] = "Your email do not match.. whatever it need to match"; 	// TEXT ERROR	
-$arrayError[1][2] = "error";			// BOX COLOR
-
+$arrayError[0] = array(
+  0 => '#email' , // Field ID
+  1 => 'Your email do not match.. whatever it need to match' , // Error Message
+  2 => 'error' // Box Color
+);
+$arrayError[1] = array(
+  0 => '#age' , // FieldID
+  1 => 'Your email do not match.. whatever it need to match' , // Error Message
+  2 => 'error' // Box Color
+);
 
 
 $isValidate = true;  // RETURN TRUE FROM VALIDATING, NO ERROR DETECTED
-/* RETTURN ARRAY FROM YOUR VALIDATION  */
-
+/* RETURN ARRAY FROM YOUR VALIDATION  */
 
 /* THIS NEED TO BE IN YOUR FILE NO MATTERS WHAT */
-if($isValidate == true){
-	echo "true";
+if( $isValidate==true ){
+	echo 'true';
 }else{
 	echo '{"jsonValidateReturn":'.json_encode($arrayError).'}';		// RETURN ARRAY WITH ERROR
 }
+
 ?>
--- validateUser.php
+++ validateUser.php
@@ -1,32 +1,34 @@
 <?php
 
 /* RECEIVE VALUE */
-$validateValue=$_POST['validateValue'];
-$validateId=$_POST['validateId'];
-$validateError=$_POST['validateError'];
+$validateValue = $_POST['validateValue'];
+$validateId    = $_POST['validateId'];
+$validateError = $_POST['validateError'];
 
+/* RETURN VALUE */
+$arrayToJs = array();
+$arrayToJs[0] = $validateId;
+$arrayToJs[1] = $validateError;
 
+if( $validateValue=='karnius' ){
 
+ // Valid
+ // Return TRUE
+	$arrayToJs[2] = 'true';
+ // Return array with Success
+  echo '{"jsonValidateReturn":'.json_encode( $arrayToJs ).'}';
 
-
-
-
-	/* RETURN VALUE */
-	$arrayToJs = array();
-	$arrayToJs[0] = $validateId;
-	$arrayToJs[1] = $validateError;
-
-if($validateValue =="karnius"){		// validate??
-	$arrayToJs[2] = "true";			// RETURN TRUE
-	echo '{"jsonValidateReturn":'.json_encode($arrayToJs).'}';			// RETURN ARRAY WITH success
 }else{
-	for($x=0;$x<1000000;$x++){
-		if($x == 990000){
-			$arrayToJs[2] = "false";
-			echo '{"jsonValidateReturn":'.json_encode($arrayToJs).'}';		// RETURN ARRAY WITH ERROR
+
+ // Invalid
+	for( $x=0 ; $x<1000000 ; $x++ ){
+		if( $x==990000 ){
+			$arrayToJs[2] = 'false';
+     // Return array with Error
+			echo '{"jsonValidateReturn":'.json_encode($arrayToJs).'}';
 		}
 	}
-	
+
 }
 
 ?>
